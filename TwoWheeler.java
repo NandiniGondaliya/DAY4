67d6bc19f224b989ad76e60a3a298c2eb01b1b05
NandiniGondaliya <74456852+NandiniGondaliya@users.noreply.github.com>
2023-02-13 15:55:15 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,25 @@
+
+class TwoWheeler extends Vehicle{
+
+	public TwoWheeler(String make, String vehicleNumber, String fuelType, Integer fuelCapacity, Integer cc) {
+		super(make, vehicleNumber, fuelType, fuelCapacity, cc);
+		
+	}
+
+	private String KickStartAvailable;
+	
+	public String isKickStartAvailable() {
+		return KickStartAvailable;
+	}
+
+	public void setKickStartAvailable(String kickStartAvailable) {
+		this.KickStartAvailable = kickStartAvailable;
+	}
+
+	void displayDetailInfo()
+	{
+		
+		System.out.println("---Detail Information---");
+		System.out.println("Kick Start Available:"+this.KickStartAvailable);
+	}
+}
